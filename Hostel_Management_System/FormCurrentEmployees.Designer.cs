7efdacf0eab4838b4d15dbc00d0eb8a598a817a4
Fahad Hasan <hasan.fahad67@outlook.com>
2025-06-07 21:40:30 +0600

    first commit

--- Hostel_Management_System/FormCurrentEmployees.Designer.cs
+++ Hostel_Management_System/FormCurrentEmployees.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.IndianRed;
+            this.panel1.Location = new System.Drawing.Point(421, 149);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(281, 169);
+            this.panel1.TabIndex = 1;
+            // 
+            // FormCurrentEmployees
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormCurrentEmployees";
             this.Text = "FormCurrentEmployees";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
